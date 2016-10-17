81cd3cb7ee1a497cf7634a206db6379916e478fb
haoxin <coderhaoxin@outlook.com>
2016-10-17 11:11:05 +0800

    build - add coverage

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+cover:
+	go test -coverprofile=coverage.out
+	go tool cover --html=coverage.out -o coverage.html
+
+.PHONY: cover
--- conn.go
+++ conn.go
@@ -4,7 +4,7 @@ import r "github.com/garyburd/redigo/redis"
 
 type Conn interface {
 	// basic
-	Del(l interface{}) error
+	Del(k interface{}) error
 	Set(k, v interface{}) error
 	Get(k interface{}) ([]byte, error)
 	Expire(k, expiration interface{}) error
